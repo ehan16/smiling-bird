1a8e531840614006159d57fd6dca688b900e2fc1
Erika Han <e.han@correo.unimet.edu.ve>
2020-02-27 09:45:11 -0400

    arreglo de error en profile edit

--- src/app/components/shared/profile/profile-edit/profile-edit.component.ts
+++ src/app/components/shared/profile/profile-edit/profile-edit.component.ts
@@ -15,6 +15,7 @@ export class ProfileEditComponent implements OnInit {
   todayDate = new Date();
   maxDate: NgbDate = new NgbDate(this.todayDate.getFullYear(), this.todayDate.getMonth(), this.todayDate.getDate() + 1);
   minDate: NgbDate = new NgbDate(this.todayDate.getFullYear() - 100, this.todayDate.getMonth(), this.todayDate.getDate());
+  date: NgbDate = this.currentUser.birth;
 
   constructor() { }
 
@@ -22,12 +23,12 @@ export class ProfileEditComponent implements OnInit {
 
     // tslint:disable-next-line: max-line-length
     // const date: NgbDate = new NgbDate(this.currentUser.birth.getFullYear(), this.currentUser.birth.getMonth(), this.currentUser.birth.getDate());
-    const date: NgbDate = this.currentUser.birth;
+    // const date: NgbDate = this.currentUser.birth;
 
     this.editForm = new FormGroup({
       name: new FormControl(this.currentUser.name, Validators.required),
       user: new FormControl(this.currentUser.user, [Validators.required, Validators.email]),
-      birthDate: new FormControl(date , Validators.required),
+      birthDate: new FormControl(this.date , Validators.required),
       gender: new FormControl(this.currentUser.gender, Validators.required),
       // shift: new FormControl
     });
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDmNJfyjM7PySxSBIXnLfq9if6tc7mPC5I',
+    authDomain: 'smiling-bird.firebaseapp.com',
+    databaseURL: 'https://smiling-bird.firebaseio.com/',
+    projectId: 'smiling-bird',
+    storageBucket: 'smiling-bird.appspot.com',
+    messagingSenderId: '574115405001',
+    appId: '1:574115405001:web:b85016cd516b6cb9e8c118',
+    measurementId: 'G-DM7DLVL86Z'
+  }
 };
