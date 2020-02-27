1b11f79aa99ab222fb6a1ae6c2e1471301946a0b
Donutman <joseivillegasvera@gmail.com>
2020-02-26 23:03:37 -0400

    Registro casi listo

--- src/app/components/shared/sign-up/sign-up.component.ts
+++ src/app/components/shared/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
+  RegisterForm: FormGroup;
+
   constructor() { }
 
   ngOnInit() {
-  }
+    this.RegisterForm = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      name: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      password2: new FormControl('', Validators.required),
+      cedula: new FormControl('', Validators.required)
+    });
 
+  }
+  onSubmit() {
+    console.log(this.RegisterForm);
+
+  }
 }
