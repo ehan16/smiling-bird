df5b3cae2afd759ea12da763cd0077d411177e3d
Erika Han <e.han@correo.unimet.edu.ve>
2020-03-11 16:11:27 -0400

    user-edit initial

--- src/app/components/shared/user-edit/user-edit.component.ts
+++ src/app/components/shared/user-edit/user-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { FirestoreService } from 'src/app/services/firestore.service';
+import { Router } from '@angular/router';
+import { FormGroup } from '@angular/forms';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +12,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  editForm: FormGroup;
+  currentUser: User;
+  editedUser: User;
+  minDate;
+  maxDate;
+
+  constructor(private  userService: UserService, private firestore: FirestoreService, private router: Router ) { }
 
   ngOnInit() {
   }
+
+  onEdit() {
+
+  }
+
 
 }
