65eeb8baef8cf320acbb6b87f5efae51b24b36b2
Rick <riicardoserrano@gmail.com>
2020-03-21 16:20:54 -0400

    paypal ready - dentistIdList not

--- src/app/components/patient/patient-view/payment/payment.component.ts
+++ src/app/components/patient/patient-view/payment/payment.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-import { IPayPalConfig, ICreateOrderRequest } from 'ngx-paypal';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
 
 import { User } from 'src/app/models/user.model';
 import { UserService } from 'src/app/services/user.service';
+import { Appointment } from 'src/app/models/appointment.model';
+import { FirestoreService } from 'src/app/services/firestore.service';
+
+  declare var paypal;
 
 @Component({
   selector: 'app-payment',
@@ -10,77 +14,70 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./payment.component.css']
 })
 export class PaymentComponent implements OnInit {
+  @ViewChild('paypal', { static: true}) paypalElement: ElementRef;
+
   showSuccess: boolean;
-  public payPalConfig?: IPayPalConfig;
 
   currentUser: User;
+  dentistIdList = [];
 
-  constructor(private userService: UserService){
+  constructor(private userService: UserService, private firestoreService: FirestoreService){
     this.currentUser = this.userService.currentUser;
   }
 
-  ngOnInit(): void {
-    this.initConfig();
-  }
+  product = {
+    price: 777.77,
+    description: 'used couch, decent condition',
+    img: 'assets/couch.jpg'
+  };
 
-  private initConfig(): void {
-    this.payPalConfig = {
-      currency: 'EUR',
-      clientId: 'sb',
-      createOrderOnClient: (data) => <ICreateOrderRequest>{
-        intent: 'CAPTURE',
-        purchase_units: [
-          {
-            amount: {
-              currency_code: 'EUR',
-              value: '9.99',
-              breakdown: {
-                item_total: {
-                  currency_code: 'EUR',
-                  value: '9.99'
+  paidFor = false;
+
+  ngOnInit(): void {
+    this.firestoreService.getAll('appointments').subscribe(data => {
+      let appointmentList = data.map(e => {
+        return {
+          id: e.payload.doc.id,
+          ...e.payload.doc.data()
+        } as Appointment;
+      });
+      
+      appointmentList = appointmentList.filter(appointment => appointment.patient === this.userService.currentUserId);
+
+      for(let appointment of appointmentList){
+        if(!this.dentistIdList.includes(appointment.dentist)){
+          this.dentistIdList.push(appointment.dentist);
+        }
+          
+      }
+      console.log(appointmentList);
+    });
+
+
+    paypal
+      .Buttons({
+        createOrder: (data, actions) => {
+          return actions.order.create({
+            purchase_units: [
+              {
+                description: this.product.description,
+                amount: {
+                  currency_code: 'USD',
+                  value: this.product.price
                 }
               }
-            },
-            items: [
-              {
-                name: 'Enterprise Subscription',
-                quantity: '1',
-                category: 'DIGITAL_GOODS',
-                unit_amount: {
-                  currency_code: 'EUR',
-                  value: '9.99',
-                },
-              }
             ]
-          }
-        ]
-      },
-      advanced: {
-        commit: 'true'
-      },
-      style: {
-        label: 'paypal',
-        layout: 'vertical'
-      },
-      onApprove: (data, actions) => {
-        console.log('onApprove - transaction was approved, but not authorized', data, actions);
-        actions.order.get().then(details => {
-          console.log('onApprove - you can get full order details inside onApprove: ', details);
-        });
-      },
-      onClientAuthorization: (data) => {
-        console.log('onClientAuthorization - you should probably inform your server about completed transaction at this point', data);
-        this.showSuccess = true;
-      },
-      onCancel: (data, actions) => {
-        console.log('OnCancel', data, actions);
-      },
-      onError: err => {
-        console.log('OnError', err);
-      },
-      onClick: (data, actions) => {
-        console.log('onClick', data, actions);
-      },
-    };
+          });
+        },
+        onApprove: async (data, actions) => {
+          const order = await actions.order.capture();
+          this.paidFor = true;
+          console.log(order);
+        },
+        onError: err => {
+          console.log(err);
+        }
+      })
+      .render(this.paypalElement.nativeElement);
   }
 }
